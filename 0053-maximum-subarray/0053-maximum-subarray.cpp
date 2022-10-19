613da7d3630d28702af0b3e2b41c005160c8b716
Vinamrata Rani <vinamrata1086@gmail.com>
2022-10-19 16:15:18 +0530

    Time: 252 ms (37.26%), Space: 67.7 MB (53.38%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -4,8 +4,9 @@ public:
        int m_sum=INT_MIN, c_sum=0;
         for(int i=0;i<nums.size();i++){
             c_sum=max(c_sum+nums[i], nums[i]);
-            if(c_sum>m_sum)
-                m_sum=c_sum;
+            // if(c_sum>m_sum)
+            //     m_sum=c_sum;
+            m_sum=max(m_sum, c_sum);
             if(c_sum<0)
                 c_sum=0;
             // if(nums.size()==1)
