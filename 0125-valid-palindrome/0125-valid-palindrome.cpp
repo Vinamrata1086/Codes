ad66d31388ed79b40454e4fed50125aa3745bb17
Vinamrata Rani <vinamrata1086@gmail.com>
2023-04-07 16:21:16 +0530

    Time: 5 ms (66.64%), Space: 7.9 MB (10.23%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -3,17 +3,18 @@ public:
     bool isPalindrome(string s) {
         string p;
         for(int i=0;i<=s.size();i++){
-            // if(s[i]<'a' || s[i]>'z' && s[i]<'0' || s[i]>'9'&& s[i]>=65 && s[i]<=92){
-            //     s.erase(i,1);
-            //     i--;}
-             // if(s[i]>=65 && s[i]<=92)
-             //    s[i]=s[i]+32;
-            if(isupper(s[i])) {
-                s[i] = tolower(s[i]);
-            }
-             if(isalnum(s[i])) {
+            if(s[i]>=65 && s[i]<=92)
+               s[i]=s[i]+32;
+            if(s[i]>='a' && s[i]<='z' || s[i]>='0' && s[i]<='9'){
                 p.push_back(s[i]);
-            }
+                }
+
+            // if(isupper(s[i])) {
+            //     s[i] = tolower(s[i]);
+            // }
+            //  if(isalnum(s[i])) {
+            //     p.push_back(s[i]);
+            // }
         }
         string rev = p;
         reverse(rev.begin(), rev.end());
