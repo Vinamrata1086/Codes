2f41274fd27554d7706ec8cf8911e288b30a658e
Vinamrata Rani <vinamrata1086@gmail.com>
2023-04-24 16:55:47 +0530

    Added solution - LeetHub

--- Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
+++ Move all zeroes to end of array - GFG/move-all-zeroes-to-end-of-array.cpp
@@ -11,8 +11,8 @@ public:
 	    int j=0;
 	    for(int i=0;i<n;i++){
 	        if(arr[i]!=0){
-	            swap(arr[i], arr[j]);
-	            j++;
+	            swap(arr[i], arr[j++]);
+	            //j++;
 	        }
 	    }
 	}
