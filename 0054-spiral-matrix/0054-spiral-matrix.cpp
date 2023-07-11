1cb0e48f128fb7a45b8088126858177d5d10d899
Vinamrata Rani <vinamrata1086@gmail.com>
2023-07-11 14:55:52 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (21.86%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -34,37 +34,37 @@ public:
 //         }
 //         return v;
         
-        vector<int> result;
-        int rows = matrix.size(), cols = matrix[0].size();
-        int left = 0, right = cols-1, top = 0, bottom = rows-1;
+        vector<int> v;
+        int n = matrix.size(), m = matrix[0].size();
+        int left = 0, right = m-1, top = 0, bottom = n-1;
         
         while (left <= right && top <= bottom) {
             for (int i = left; i <= right; i++) {
-                result.push_back(matrix[top][i]);
+                v.push_back(matrix[top][i]);
             }
             top++;
             
             for (int i = top; i <= bottom; i++) {
-                result.push_back(matrix[i][right]);
+                v.push_back(matrix[i][right]);
             }
             right--;
             
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
-                    result.push_back(matrix[bottom][i]);
+                    v.push_back(matrix[bottom][i]);
                 }
                 bottom--;
             }
             
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
-                    result.push_back(matrix[i][left]);
+                    v.push_back(matrix[i][left]);
                 }
                 left++;
             }
         }
         
-        return result;
+        return v;
         
     }
 };
