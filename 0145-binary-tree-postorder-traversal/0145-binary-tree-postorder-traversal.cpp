ec1ef3780a2854092c55a362b85010914e4c743b
Vinamrata Rani <vinamrata1086@gmail.com>
2023-07-14 15:15:45 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (23.54%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -11,30 +11,42 @@
  */
 class Solution {
 public:
-    vector<int> postorderTraversal(TreeNode* root) {
-        vector<int> v;
-        stack<TreeNode*> s1;
-        stack<TreeNode*> s2;
-        if(root==NULL)
-            return v;
-        s1.push(root);
-       while(!s1.empty()){
-           TreeNode* temp = s1.top();
-           s1.pop();
-           s2.push(temp);
-           if(temp->left!=NULL){
-               s1.push(temp->left);
-           }
-           if(temp->right!=NULL){
-               s1.push(temp->right);
-           }
-       }
-        
-        while(!s2.empty()){
-            TreeNode* temp = s2.top();
-                s2.pop();
-            v.push_back(temp->val);
+    vector<int> v;
+    void postorder(TreeNode* &root){
+        if(root==NULL){
+            return ;
         }
+        postorder(root->left);
+        postorder(root->right);
+        v.push_back(root->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        postorder(root);
         return v;
+//------------------using stack------------------------------
+        //         vector<int> v;
+//         stack<TreeNode*> s1;
+//         stack<TreeNode*> s2;
+//         if(root==NULL)
+//             return v;
+//         s1.push(root);
+//        while(!s1.empty()){
+//            TreeNode* temp = s1.top();
+//            s1.pop();
+//            s2.push(temp);
+//            if(temp->left!=NULL){
+//                s1.push(temp->left);
+//            }
+//            if(temp->right!=NULL){
+//                s1.push(temp->right);
+//            }
+//        }
+        
+//         while(!s2.empty()){
+//             TreeNode* temp = s2.top();
+//                 s2.pop();
+//             v.push_back(temp->val);
+//         }
+//         return v;
     }
 };
