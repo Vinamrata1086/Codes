2cb5320754105a82dfa64a56f7a8990e5cb97db0
Vinamrata Rani <vinamrata1086@gmail.com>
2023-07-17 23:30:18 +0530

    Added solution - LeetHub

--- First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
+++ First non-repeating character in a stream - GFG/first-nonrepeating-character-in-a-stream.cpp
@@ -7,26 +7,45 @@ class Solution {
 	public:
 		string FirstNonRepeating(string A){
 		    // Code here
-		    queue<char> q;
-            vector<int> f(26, 0);
-            string ans = "";
+		  //  queue<char> q;
+    //         vector<int> v(26, 0);
+    //         string ans = "";
             
-            for(auto i : A){
-                ++f[i - 'a'];
+    //         for(auto i : A){
+    //             ++f[i - 'a'];
                 
-                if(f[i - 'a'] == 1)
-                    q.push(i);
+    //             if(f[i - 'a'] == 1)
+    //                 q.push(i);
                     
-                while(q.size() and f[q.front() - 'a'] > 1)
-                    q.pop();
+    //             while(q.size() and f[q.front() - 'a'] > 1)
+    //                 q.pop();
                     
-                if(q.size())
-                    ans += q.front();
-                else
-                    ans += '#';
-            }
+    //             if(q.size())
+    //                 ans += q.front();
+    //             else
+    //                 ans += '#';
+    //         }
             
-            return ans;
+    //         return ans;
+            
+            
+            queue<char> q;
+            map<char, int> m;
+            string s;
+            
+            for(int i=0;i<A.size();i++){
+                char c=A[i];
+                m[c]++;
+                if(m[c]==1)
+                    q.push(c);
+                while(!q.empty() && m[q.front()]!=1)
+                    q.pop();
+                if(q.empty())
+                    s.push_back('#');
+                else
+                    s.push_back(q.front());
+            }
+            return s;
 		}
 
 };
